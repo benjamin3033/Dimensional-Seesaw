ab5dfeecc89f83c62da8cd9f42ed36c11157a86a
Ben <benjaminjsaddler@gmail.com>
2021-11-02 08:59:50 +1300

    Bullet Shooting start

---
+++ Assets/BS/Scripts/WeaponShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShooting : MonoBehaviour
+{
+    public GameObject bullet = null;
+    public Transform barrel = null;
+
+    public float bulletSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            GameObject clone = Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
+            clone.GetComponent<Rigidbody>().AddForce(barrel.transform.position * bulletSpeed);
+        }
+    }
+}
