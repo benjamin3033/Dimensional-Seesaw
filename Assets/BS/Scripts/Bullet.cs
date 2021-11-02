3065abb2ecc3d32f3b9f2fc35b1daba92adac191
Ben <benjaminjsaddler@gmail.com>
2021-11-02 15:27:42 +1300

    Bullet Shooting Continued

---
+++ Assets/BS/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeTime = 10f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Enemy":
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+                break;
+
+            case "Object":
+                Destroy(gameObject);
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        BulletLifeTime();
+    }
+
+    void BulletLifeTime()
+    {
+        if (lifeTime > 0)
+        {
+            lifeTime -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/BS/Scripts/WeaponShooting.cs
+++ Assets/BS/Scripts/WeaponShooting.cs
@@ -4,24 +4,59 @@ using UnityEngine;
 
 public class WeaponShooting : MonoBehaviour
 {
-    public GameObject bullet = null;
-    public Transform barrel = null;
+    public GameObject energyBall = null, bolt = null, laserGun = null, crossBow = null;
+    public Transform laserBarrel = null, crossBowBarrel = null;
 
-    public float bulletSpeed = 10f;
+    GameObject bullet;
+    Transform barrel;
+
+    public float bulletSpeed = 1000f;
+
+    bool isLaserWeapon = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bullet = bolt;
+        barrel = crossBowBarrel;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ReactToInput();
+        
+        if(isLaserWeapon)
+        {
+            laserGun.gameObject.SetActive(true);
+            crossBow.gameObject.SetActive(false);
+            barrel = laserBarrel;
+            bullet = energyBall;
+        }
+        else
+        {
+            laserGun.gameObject.SetActive(false);
+            crossBow.gameObject.SetActive(true);
+            barrel = crossBowBarrel;
+            bullet = bolt;
+        }
+    }
+    void ReactToInput()
+    {
         if (Input.GetButtonUp("Fire1"))
         {
-            GameObject clone = Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
-            clone.GetComponent<Rigidbody>().AddForce(barrel.transform.position * bulletSpeed);
+            FireBullet();
         }
+
+        if(Input.GetButtonUp("WeaponSwitch"))
+        {
+            isLaserWeapon = !isLaserWeapon;
+        }
+    }
+
+    void FireBullet()
+    {
+        GameObject clone = Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
+        clone.GetComponent<Rigidbody>().AddForce(barrel.transform.forward * bulletSpeed);
     }
 }
