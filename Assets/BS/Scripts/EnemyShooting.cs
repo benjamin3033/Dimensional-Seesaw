5acd36e47518508117ce12e0b190101c495a2203
Ben <benjaminjsaddler@gmail.com>
2021-11-15 15:13:28 +1300

    Enemy shooting, Enemy health

--- Assets/BS/Scripts/EnemyShooting.cs
+++ Assets/BS/Scripts/EnemyShooting.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemyShooting : MonoBehaviour
 {
+    public Transform ShotStart = null;
+    public GameObject projectile = null;
+
+    public float ProjectileSpeed = 1f;
+    public float ProjectileRate = 1f;
+
+    bool canShoot = true;
+    float shootingTimer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shootingTimer = ProjectileRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(canShoot)
+        {
+            GameObject clone = Instantiate(projectile, ShotStart.transform.position, ShotStart.transform.rotation);
+            clone.GetComponent<Rigidbody>().AddForce(ShotStart.transform.forward * ProjectileSpeed);
+            canShoot = false;
+        }
+        else
+        {
+            if (shootingTimer > 0)
+            {
+                shootingTimer -= Time.deltaTime;
+            }
+            else
+            {
+                shootingTimer = ProjectileRate;
+                canShoot = true;
+            }
+        }
     }
 }
